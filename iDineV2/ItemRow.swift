a839555d67626f9defaa40bca83646b0eaad35be
Fern Botelho <fmb4800@gmail.com>
2024-08-13 21:35:03 -0400

    Add ItemRow.swift default SwiftUIView.

---
+++ iDineV2/ItemRow.swift
@@ -0,0 +1,18 @@
+//
+//  ItemRow.swift
+//  iDineV2
+//
+//  Created by Super Grover on 2024-08-13.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemRow()
+}
