fb1a3f46c2e239d4fb26fff7b1f92ed4028e2c03
Fern Botelho <fmb4800@gmail.com>
2024-08-14 13:50:26 -0400

    In OrderView, add '@EnvironmentObject var order: Order' property.

--- iDineV2/OrderView.swift
+++ iDineV2/OrderView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct OrderView: View {
+    @EnvironmentObject var order: Order
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
