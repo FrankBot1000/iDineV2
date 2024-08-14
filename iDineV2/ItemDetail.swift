67c8a3f35a9e4cb020e68b61f5d5852fb9177eac
Fern Botelho <fmb4800@gmail.com>
2024-08-14 11:14:06 -0400

    Add ItemDetail.swift SwiftUI View file.

---
+++ iDineV2/ItemDetail.swift
@@ -0,0 +1,18 @@
+//
+//  ItemDetail.swift
+//  iDineV2
+//
+//  Created by Super Grover on 2024-08-14.
+//
+
+import SwiftUI
+
+struct ItemDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemDetail()
+}
