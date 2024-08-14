396b1278ca0ee1d16745fbb2231abf58db4370fa
Fern Botelho <fmb4800@gmail.com>
2024-08-14 11:12:05 -0400

    In ContentView, wrap ItemRow(item: item) within the 'label: { } ' of a NavigationLink { Text(item.name) }.

--- iDineV2/ContentView.swift
+++ iDineV2/ContentView.swift
@@ -21,7 +21,12 @@ struct ContentView: View {
                     ForEach(menu) { section in
                         Section(section.name) {
                             ForEach(section.items) { item in
-                                ItemRow(item: item)
+                                NavigationLink {
+                                    Text(item.name)
+                                } label: {
+                                    ItemRow(item: item)
+                                }
+                                
                             }
                         }
                     }
