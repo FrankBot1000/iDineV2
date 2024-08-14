82dabb7d138251eb5e9d4012bd7965c0667518f9
Fern Botelho <fmb4800@gmail.com>
2024-08-13 21:32:25 -0400

    In ContentView, add .listStyle(.grouped) modifier to List.

--- iDineV2/ContentView.swift
+++ iDineV2/ContentView.swift
@@ -27,6 +27,7 @@ struct ContentView: View {
                     }
                 }
                 .navigationTitle("Menu")
+                .listStyle(.grouped)
             }
             
         }
