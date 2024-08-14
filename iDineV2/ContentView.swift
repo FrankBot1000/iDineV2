564b20215785940b7346bc7b5c129e28f5529ead
Fern Botelho <fmb4800@gmail.com>
2024-08-13 20:42:16 -0400

    Initial Commit

---
+++ iDineV2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iDineV2
+//
+//  Created by Super Grover on 2024-08-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iDineV2/iDineV2App.swift
@@ -0,0 +1,17 @@
+//
+//  iDineV2App.swift
+//  iDineV2
+//
+//  Created by Super Grover on 2024-08-13.
+//
+
+import SwiftUI
+
+@main
+struct iDineV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
