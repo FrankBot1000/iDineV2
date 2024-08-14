6460d8e41169439e9c3381c22b310f6e195483d3
Fern Botelho <fmb4800@gmail.com>
2024-08-14 11:20:59 -0400

    In ItemDetail.swift, update 'body' with a VStack that includes an Image and Text, and include a .navigationTitle modifier.

--- iDineV2/ItemDetail.swift
+++ iDineV2/ItemDetail.swift
@@ -11,7 +11,11 @@ struct ItemDetail: View {
     let item: MenuItem
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(item.mainImage)
+            Text(item.description)
+        }
+        .navigationTitle(item.name)
     }
 }
 
