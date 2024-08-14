ddf5c2d6e4703a81cd6d8044e3f959d95cb1d7b8
Fern Botelho <fmb4800@gmail.com>
2024-08-14 13:49:32 -0400

    Add OrderView.swift file.

---
+++ iDineV2/OrderView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderView.swift
+//  iDineV2
+//
+//  Created by Super Grover on 2024-08-14.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderView()
+}
