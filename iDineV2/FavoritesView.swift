e2dbb3e5b30b99682d975718b6a5832e7ef3dbd3
Fern Botelho <fmb4800@gmail.com>
2024-08-14 17:50:20 -0400

    Add FavoritesView.swift SwiftUI View file.

---
+++ iDineV2/FavoritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesView.swift
+//  iDineV2
+//
+//  Created by Super Grover on 2024-08-14.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesView()
+}
