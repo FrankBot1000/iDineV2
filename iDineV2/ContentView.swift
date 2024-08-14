e0be208638121761c4952b155d342b313addc2d5
Fern Botelho <fmb4800@gmail.com>
2024-08-13 21:06:08 -0400

    In ContentView, add navigationTitle modifier to List, within NavigationStack.

--- iDineV2/ContentView.swift
+++ iDineV2/ContentView.swift
@@ -19,6 +19,7 @@ struct ContentView: View {
                     Text("Hello World")
                     Text("Hello World")
                 }
+                .navigationTitle("Menu")
             }
             
         }
