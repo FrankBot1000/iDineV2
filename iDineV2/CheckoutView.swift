cc1329731706c936f590157460144c80a0171186
Fern Botelho <fmb4800@gmail.com>
2024-08-14 14:47:04 -0400

    Add CheckoutView.swift SwiftUI View file.

---
+++ iDineV2/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckoutView.swift
+//  iDineV2
+//
+//  Created by Super Grover on 2024-08-14.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView()
+}
