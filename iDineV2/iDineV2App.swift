d0cb0c810fd00ed291555999c83edc55ec264e24
Fern Botelho <fmb4800@gmail.com>
2024-08-14 14:12:11 -0400

    In iDineVSQpp, 'body', replace ContentView() with MainView().

--- iDineV2/iDineV2App.swift
+++ iDineV2/iDineV2App.swift
@@ -13,7 +13,7 @@ struct iDineV2App: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
                 .environmentObject(order)
         }
     }
