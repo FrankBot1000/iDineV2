211782ebb3e7125c594fc0b75d5f9b12c4b72d33
Fern Botelho <fmb4800@gmail.com>
2024-08-15 00:29:56 -0400

    In FavoritesView, refactor: in NavigationStack, List, replace Sections with a single ForEach that iterates over all 'itemTypes' and shows the itemType Section if favorites for that section is not empty.

--- iDineV2/FavoritesView.swift
+++ iDineV2/FavoritesView.swift
@@ -13,57 +13,12 @@ struct FavoritesView: View {
     var body: some View {
         NavigationStack {
             List {
-                if !favorites.items.filter({$0.itemType == .breakfast}).isEmpty {
-                    Section("Breakfast") {
-                        ForEach(favorites.items.filter{$0.itemType == .breakfast}) { favorite in
-                            HStack {
-                                NavigationLink(value: favorite) {
-                                    ItemRow(item: favorite)
-                                }
-                            }
-                            
-                        }
+                let itemTypes = ItemType.allCases
+                ForEach(itemTypes, id: \.self) { itemType in
+                    if !favorites.items.filter({$0.itemType == itemType}).isEmpty {
+                        FavoritesSection(itemType: itemType, favorites: favorites)
                     }
                 }
-                
-                if !favorites.items.filter({$0.itemType == .mains}).isEmpty {
-                    Section("Mains") {
-                        ForEach(favorites.items.filter{$0.itemType == .mains}) { favorite in
-                            HStack {
-                                NavigationLink(value: favorite) {
-                                    ItemRow(item: favorite)
-                                }
-                            }
-                            
-                        }
-                    }
-                }
-                
-                if !favorites.items.filter({$0.itemType == .dessert}).isEmpty {
-                    Section("Desserts") {
-                        ForEach(favorites.items.filter{$0.itemType == .dessert}) { favorite in
-                            HStack {
-                                NavigationLink(value: favorite) {
-                                    ItemRow(item: favorite)
-                                }
-                            }
-                            
-                        }
-                    }
-                }
-                
-                if !favorites.items.filter({$0.itemType == .drinks}).isEmpty {
-                    Section("Drinks") {
-                        ForEach(favorites.items.filter{$0.itemType == .drinks}) { favorite in
-                            HStack {
-                                NavigationLink(value: favorite) {
-                                    ItemRow(item: favorite)
-                                }
-                            }
-                        }
-                    }
-                }
-
             }
             .navigationDestination(for: MenuItem.self, destination: { menuItem in
                 ItemDetail(item: menuItem, isFavorite: true)
