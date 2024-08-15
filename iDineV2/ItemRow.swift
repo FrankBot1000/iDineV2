54e1ab59595665b9b014ab992914b4bd390f4de9
Fern Botelho <fmb4800@gmail.com>
2024-08-15 13:46:43 -0400

    Update "Created by" and "Copyright" in comments; include me instead of alias 'Super Grover', and include original "Created by..." and "Copyright".

--- iDineV2/ItemRow.swift
+++ iDineV2/ItemRow.swift
@@ -2,8 +2,13 @@
 //  ItemRow.swift
 //  iDineV2
 //
-//  Created by Super Grover on 2024-08-13.
+//  Created by Fern Botelho on 2024-08-14.
+//  Copyright © 2019 Fern Botelho. All rights reserved.
 //
+//  A version of the "iDine" App from the book "SwiftUI by Example" by Paul Hudson that includes code challenges.
+//
+//  Original version of "iDine" created by Paul Hudson on 23/06/2019.
+//  Copyright © 2019 Hacking with Swift. All rights reserved.
 
 import SwiftUI
 
--- iDineV2/Menu.swift
+++ iDineV2/Menu.swift
@@ -2,9 +2,13 @@
 //  Menu.swift
 //  iDine
 //
-//  Created by Paul Hudson on 27/06/2019.
-//  Copyright © 2019 Hacking with Swift. All rights reserved.
+//  Created by Fern Botelho on 2024-08-14.
+//  Copyright © 2019 Fern Botelho. All rights reserved.
 //
+//  A version of the "iDine" App from the book "SwiftUI by Example" by Paul Hudson that includes code challenges.
+//
+//  Original version of "iDine" created by Paul Hudson on 23/06/2019.
+//  Copyright © 2019 Hacking with Swift. All rights reserved.
 
 import SwiftUI
 
